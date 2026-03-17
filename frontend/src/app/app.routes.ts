aaab1de5c469ccdfef490dcefd3f6a9ba0692ebf
Miguel Uve <jm.vilata@yurest.com>
2026-03-17 08:43:07 +0100

    Layout and fix angular hosts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
 import { RegisterComponent } from './features/user/register/register.component';
 
 export const routes: Routes = [
-  { path: '', component: RegisterComponent },
-  { path: 'register', component: RegisterComponent },
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'register', pathMatch: 'full' },
+      { path: 'register', component: RegisterComponent },
+    ],
+  },
 ];
