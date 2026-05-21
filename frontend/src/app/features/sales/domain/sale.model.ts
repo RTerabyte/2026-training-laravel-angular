39040d6d32d17464a580ad5b77a5cde3cca1ca4a
RTerabyte <rodry.ter@gmail.com>
2026-05-21 09:29:31 +0200

    add final ticket

---
+++ frontend/src/app/features/sales/domain/sale.model.ts
@@ -0,0 +1,11 @@
+export interface Sale {
+  id: string;
+  restaurant_id: string;
+  order_id: string;
+  user_id: string;
+  ticket_number: number | null;
+  value_date: string;
+  total: number;
+  created_at: string;
+  updated_at: string;
+}
